496332f13ce6ec3d50cde2c9626ec4de05bedc04
Pascal Racine-Venne <pascalracinevenne@gmail.com>
2021-05-10 17:00:54 -0400

    trying to implement API call

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,6 @@
+require 'json'
+require 'open-uri'
+
 class GamesController < ApplicationController
   def new
     @letters = ('a'..'z').to_a
@@ -6,6 +9,19 @@ class GamesController < ApplicationController
   end
 
   def score
+    @answer = @params[:word]
+    @result = api
+    # 3 case scenarios
+    # The word can’t be built out of the original grid
+    # The word is valid according to the grid, but is not a valid English word
+    # The word is valid according to the grid and is an English word
+  end
 
+    # create method for API call had require relative on top.
+  def api
+    url = "https://wagon-dictionary.herokuapp.com/#{@answer}"
+    response = URI.open(url).read
+    json = JSON.parse(response)
+    return json['found']
   end
 end
